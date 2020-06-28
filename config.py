6dc542820eb8c5aabee824648fd5e456e01929fe
Konrad Amtenbrink <39226727+konrad-amtenbrink@users.noreply.github.com>
2020-06-28 22:24:35 +0200

    Delete config.py
    .gitignore Error with config file

--- config.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-API_KEY = "YBF19XIgidKybQe2JNojpqYwKqdpqOcB5cCo6u12zPxNW65LeTg1wfQVsp40RQdd"
-
-API_SECRET = "P9Nc2llPv2qvBOR1yS22gzdEYEEl3GVv47RSqBxFrI6Teeenbw6BGnIxaewL6yDJ"
